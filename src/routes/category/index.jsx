d44bd9597d88b59706f2d3dc43dea8f590ea3c28
flvSantos15 <flvSantos300@gmail.com>
2023-09-25 21:41:03 -0300

    feat: finished section 10

---
+++ src/routes/category/index.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { useCategory } from '../../contexts/categoriesContext'
+
+import './styles.scss'
+import { ProductCard } from '../../components/product-card'
+
+export default function Category() {
+  const { category } = useParams()
+  const { categories } = useCategory()
+
+  const [products, setProducts] = useState(categories[category])
+
+  useEffect(() => {
+    setProducts(categories[category])
+  }, [category, categories])
+
+  return (
+    <div className="category-route-container">
+      {products &&
+        products.map((product) => {
+          return <ProductCard key={product.id} product={product} />
+        })}
+    </div>
+  )
+}
