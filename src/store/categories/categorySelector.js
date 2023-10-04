52d53d52d53e0546ce856b557608717f679d9d82
flvSantos15 <flvSantos300@gmail.com>
2023-10-03 23:40:31 -0300

    doc: reselector

--- src/store/categories/categorySelector.js
+++ src/store/categories/categorySelector.js
@@ -1,6 +1,18 @@
-export const selectCategory = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category
-    acc[title.toLowerCase()] = items
-    return acc
-  }, {})
+import { createSelector } from 'reselect'
+
+const selectCategoryReducer = (state) => state.categories
+
+export const selecteCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategory = createSelector(
+  [selecteCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category
+      acc[title.toLowerCase()] = items
+      return acc
+    }, {})
+)
